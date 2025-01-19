15bd860ab4f323783f8976ab062d036c9aa40161
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-19 17:24:37 +0530

    ff

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -40,41 +40,41 @@ variable "secrets" {
       #   DB_HOST     = "mongodb-dev.azdevops.shop"
       #   SCHEMA_FILE = "db/master-data.js"
       }
-      # user = {
-      #   MONGO     = "true"
-      #   REDIS_URL = "redis://redis-dev.azdevops.shop:6379"
-      #   MONGO_URL = "mongodb://mongodb-dev.azdevops.shop:27017/users"
-      # }
-      # cart = {
-      #   REDIS_HOST     = "redis-dev.azdevops.shop"
-      #   CATALOGUE_HOST = "catalogue-dev.azdevops.shop"
-      #   CATALOGUE_PORT = "8080"
-      # }
-      # shipping = {
-      #   CART_ENDPOINT = "cart-dev.azdevops.shop:8080"
-      #   DB_HOST       = "mysql-dev.azdevops.shop"
-      #   DB_TYPE       = "mysql"
-      #   APP_GIT_URL   = "https://github.com/roboshop-devops-project-v3/shipping"
-      #   DB_HOST       = "mysql-dev.azdevops.shop"
-      #   DB_USER       = "root"
-      #   DB_PASS       = "RoboShop@1"
-      # }
-      # payment = {
-      #   CART_HOST = "cart-dev.azdevops.shop"
-      #   CART_PORT = "8080"
-      #   USER_HOST = "user-dev.azdevops.shop"
-      #   USER_PORT = "8080"
-      #   AMQP_HOST = "rabbitmq-dev.azdevops.shop"
-      #   AMQP_USER = "roboshop"
-      #   AMQP_PASS = "roboshop123"
-      # }
-      # mysql = {
-      #   MYSQL_ROOT_PASSWORD = "RoboShop@1"
-      # }
-      # rabbitmq = {
-      #   AMQP_USER = "roboshop"
-      #   AMQP_PASS = "roboshop123"
-      # }
+      user = {
+        MONGO     = "true"
+        REDIS_URL = "redis://redis-dev.azdevops.shop:6379"
+        MONGO_URL = "mongodb://mongodb-dev.azdevops.shop:27017/users"
+      }
+      cart = {
+        REDIS_HOST     = "redis-dev.azdevops.shop"
+        CATALOGUE_HOST = "catalogue-dev.azdevops.shop"
+        CATALOGUE_PORT = "8080"
+      }
+      shipping = {
+        CART_ENDPOINT = "cart-dev.azdevops.shop:8080"
+        DB_HOST       = "mysql-dev.azdevops.shop"
+        DB_TYPE       = "mysql"
+        APP_GIT_URL   = "https://github.com/roboshop-devops-project-v3/shipping"
+        DB_HOST       = "mysql-dev.azdevops.shop"
+        DB_USER       = "root"
+        DB_PASS       = "RoboShop@1"
+      }
+      payment = {
+        CART_HOST = "cart-dev.azdevops.shop"
+        CART_PORT = "8080"
+        USER_HOST = "user-dev.azdevops.shop"
+        USER_PORT = "8080"
+        AMQP_HOST = "rabbitmq-dev.azdevops.shop"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+      mysql = {
+        MYSQL_ROOT_PASSWORD = "RoboShop@1"
+      }
+      rabbitmq = {
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
     }
   }
 }
